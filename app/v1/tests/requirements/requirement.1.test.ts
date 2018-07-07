6c577980ad2047e3def7ec255ac3129218484f67
Scott <scott2345@gmail.com>
2018-07-07 10:22:17 -0700

    testing taking out coverage.

--- app/v1/tests/requirements/requirement.1.test.ts
+++ app/v1/tests/requirements/requirement.1.test.ts
@@ -41,7 +41,7 @@ describe('Requirement Creation Robustness', () => {
         }
         catch (err) {
             expect(err.response.statusCode).toBe(HttpStatus.BAD_REQUEST);
-            expect(err.response.body.error).toContain('Requirement does not exist');
+            expect(err.response.body).toHaveProperty('error');
         }
     });
 
@@ -61,7 +61,7 @@ describe('Requirement Creation Robustness', () => {
         }
         catch (err) {
             expect(err.response.statusCode).toBe(HttpStatus.BAD_REQUEST);
-            expect(err.response.body.error).toContain('Data field missing');
+            expect(err.response.body).toHaveProperty('error');
         }
     });
 
@@ -81,7 +81,7 @@ describe('Requirement Creation Robustness', () => {
         }
         catch (err) {
             expect(err.response.statusCode).toBe(HttpStatus.BAD_REQUEST);
-            expect(err.response.body.error).toContain('Data field must contain an object');
+            expect(err.response.body).toHaveProperty('error');
         }
     });
 });
@@ -119,6 +119,29 @@ describe('Requirement Creation', async () => {
         expect(response.body.data.name).toBe('It doesnt matter what my name is');
         expect(response.body.data.description).toBe('Behold, this is REQ001');
     });
+
+    test('Verify we cannot create a duplicate requirement', async () => {
+        const options = {
+            method: 'PUT',
+            uri: server_location + '/requirements/REQ001',
+            body: {
+                data: {
+                    name: 'A different name',
+                    description: 'Behold, this is REQ001s doppleganger'
+                }
+            },
+            resolveWithFullResponse: true,
+            json: true
+        };
+
+        try {
+            const response = await request.put(options);
+        }
+        catch (err) {
+            expect(err.response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+            expect(err.response.body).toHaveProperty('error');
+        }
+    });
 });
 
 afterAll(async() => {
@@ -127,92 +150,3 @@ afterAll(async() => {
         await mongod.stop();
     }
 });
-
-
-// mocha.describe('Requirements Creation - Basic', function() {
-//     mocha.before(function() {
-//         const promise = startServer(database);
-//         return promise.then((connection: any) => {
-//         })
-//         .catch((reason: any) => {
-//             console.error('ERROR: Could not connect to MongoDB... Aborting');
-//             process.exit(1);
-//         });
-//     });
-
-//     mocha.it('Should return an error when a non-existent requirement is accessed', function(done) {
-//         const req = request.get(server_location + '/requirements/REQ001', function(err, res, body) {
-//             chai.expect(res.statusCode).to.equal(HttpStatus.BAD_REQUEST);
-//             done();
-//         });
-//     });
-
-//     mocha.it('Should return a created response when created', function(done) {
-//         const data = { data: 'This is a sample requirement' };
-//         const options: request.CoreOptions = {
-//             method: 'PUT',
-//             body: data,
-//             json: true
-//           };
-
-//         request.put(server_location + '/requirements/REQ001', options, function(err, res, body) {
-//             chai.expect(res.statusCode).to.equal(HttpStatus.CREATED);
-//             done();
-//         });
-//     });
-
-//     mocha.it('Should reject the creation of a new requirement with an ID that matches an existing one', function (done) {
-//         const data = { description: 'This is a sample requirement' };
-//         const options: request.CoreOptions = {
-//             method: 'POST',
-//             body: data,
-//             json: true
-//           };
-
-//         request.post(server_location + '/requirements/create/REQ001', options, function(err, res, body) {
-
-//             chai.expect(body.data).to.be.undefined;
-//             chai.expect(body.id).to.be.undefined;
-//             chai.expect(body._id).to.be.undefined;
-//             chai.expect(body.deleted).to.be.undefined;
-//             chai.expect(body.history).to.be.undefined;
-
-//             // Error params
-//             chai.expect(body.error.code).to.equal(11000);
-//             chai.expect(body.error.message).contains('duplicate key error');
-
-//             done();
-//         });
-//     });
-
-//     mocha.it('Should create a requirement with blank data if the body is empty', function (done) {
-//         const options: request.CoreOptions = {
-//             method: 'POST',
-//             json: true
-//           };
-
-//           const req = request.post(server_location + '/requirements/create/REQ002', options, function(err, res, body) {
-//             chai.expect(body.data).to.deep.equal({});
-//             chai.expect(body.id).to.equal('REQ002');
-//             chai.expect(body._id).to.exist;
-//             chai.expect(body.deleted).to.equal(false);
-//             chai.expect(body.history).to.have.length(1);
-//             done();
-//         });
-//     });
-
-//     mocha.after(function() {
-//         const promise = mongoose.connection.dropDatabase();
-
-//         promise.then((value) => {
-//             stopServer();
-//             return promise;
-//         })
-//         .catch ((reason) => {
-//             console.log(reason);
-//             stopServer();
-//             return promise;
-//         });
-
-//     });
-// });
